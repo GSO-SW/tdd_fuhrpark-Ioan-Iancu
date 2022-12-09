10afb770637a5b13d6c9b7d4061f2099ae827636
Ioan-Iancu <ionut.iancu01@gmail.com>
2022-12-09 12:34:54 +0100

    Den Program für den Test Fahre-SteigertKilometerstand wurde implementiert und funktioniert

---
+++ Fuhrparkverwaltung/Auto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Fuhrparkverwaltung
+{
+	public class Auto
+	{
+		private int kilometernStand;
+
+		public Auto(int kilometernStand)
+		{
+			this.kilometernStand = kilometernStand;
+		}
+
+		public int Kilometerstand
+		{
+			get
+			{
+				return this.kilometernStand;
+			}
+		}
+
+		public void Fahren(int streckeInKilomertern)
+		{
+			kilometernStand += streckeInKilomertern;
+		}
+	}
+}
+
