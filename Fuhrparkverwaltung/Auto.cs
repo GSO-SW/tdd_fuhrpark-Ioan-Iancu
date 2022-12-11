b75c339dc47448f3eda29aadb3e43687bbe4f5d3
Ioan-Iancu <ionut.iancu01@gmail.com>
2022-12-11 16:52:45 +0100

    Den Tank wird entleert entsprechend dem Verbrauch. Test TankInhaltWirdGeleertEntsprechendDemVerbrauch ist gruen.

--- Fuhrparkverwaltung/Auto.cs
+++ Fuhrparkverwaltung/Auto.cs
@@ -4,6 +4,14 @@ namespace Fuhrparkverwaltung
 	public class Auto
 	{
 		private int kilometernStand;
+		private double tankinhaltInLiter;
+		private readonly double verbrauchInLiter;
+
+		public Auto (int kilometernStand, double tankinhaltInLiter, double verbrauchInLiter):this(kilometernStand)
+        {
+			this.tankinhaltInLiter= tankinhaltInLiter;
+			this.verbrauchInLiter = verbrauchInLiter;
+		}
 
 		public Auto(int kilometernStand)
 		{
@@ -14,16 +22,34 @@ namespace Fuhrparkverwaltung
 		{
 			get
 			{
-				return this.kilometernStand;
+				return kilometernStand;
 			}
 		}
 
 		public void Fahren(int streckeInKilomertern)
 		{
+			double gesamtVerbrauchInLiter;
 			if (streckeInKilomertern > 0)
 			{
 				kilometernStand += streckeInKilomertern;
+				gesamtVerbrauchInLiter = streckeInKilomertern / 100.0 * verbrauchInLiter;
+				tankinhaltInLiter -= gesamtVerbrauchInLiter;
+			}
+		}
 
+		public double TankinhaltInLiter
+		{
+			get
+			{
+				return tankinhaltInLiter;
+			}
+		}
+
+		public double VerbrauchInLiter
+		{
+			get
+			{
+				return verbrauchInLiter;
 			}
 		}
 
--- FuhrparkverwaltungTests/AutoTests.cs
+++ FuhrparkverwaltungTests/AutoTests.cs
@@ -33,8 +33,22 @@ namespace FuhrparkverwaltungTests
             a.Fahren(negativeStreckeInKilometern);
 
             //Assert
-
             Assert.AreEqual(25, a.Kilometerstand);
+        }
+
+        [TestMethod]
+        public void Fahren_TankInhaltWirdGeleertEntsprechendDerVerbrauch()
+        {
+            //Arrange
+            int kilometerStand = 0;
+            double verbrauchInLiter = 10.0;
+            double tankinhaltInLiter = 60.0;
+            Auto a = new Auto(kilometerStand, tankinhaltInLiter, verbrauchInLiter);
+            int streckeInKilometer = 120;
+            //Act
+            a.Fahren(streckeInKilometer);
+            //Assert
+            Assert.AreEqual(48, a.TankinhaltInLiter);
         }
     }
 }
